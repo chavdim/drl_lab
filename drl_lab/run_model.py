138d2086f8b31c6187d89ae48f70480ed33e4405
Dimitrov Chavdar <chavdar.dimitrov@BIZ0015000384.local>
2018-05-01 18:00:16 +0900

    safety

---
+++ drl_lab/run_model.py
@@ -0,0 +1,25 @@
+import numpy as np
+import keras
+import gym
+import gym_ple
+#
+from skimage.transform import rescale
+
+env_name = 'PixelCopter-v0'
+
+obsShape = [60,60,3]# cropped , rescaled 
+zoom = [1,1]
+originalDim = list(env.observation_space.shape)
+#
+t=0
+for i in originalDim[0:-1]:
+    zoom[t] = obsShape[t]/i
+    t+=1
+zc = zoom[1]
+zoom[1]=zoom[0]
+zoom[0]=zc
+
+env = gym.make(env_name)
+env.seed()
+observation = env.reset()
+observation = rescale(observation,zoom)
